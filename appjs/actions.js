857885fc32e5ebe1af35260b3e6dbed502d0b472
Eon Matrix <matrix_eon@yahoo.com>
2012-10-27 11:36:00 -0500

    N4

--- appjs/actions.js
+++ appjs/actions.js
@@ -3,13 +3,13 @@ function get_device_info(){
 	$('#device table td').eq(1).text(device.name);
     $('#device table td').eq(3).text(device.phonegap);
 	$('#device table td').eq(5).text(device.plataform);
-	$('#device table td').eq(7).text(device.version);
+	$('#device table td').eq(7).text('device.version');
 	$('#device table td').eq(7).text(device.uuid);
 }
 
 $(document).ready(function(e) {
 
     document.addEventListener('deviceready', function (){
-	get_device_info();		
+	get_device_info();	
 	},false);
 });
