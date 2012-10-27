f961e04be67861b051badac21892f7b37dd6d935
Eon Matrix <matrix_eon@yahoo.com>
2012-10-27 11:40:30 -0500

    N5

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,9 +1,9 @@
 // JavaScript Document
 function get_device_info(){
-	$('#device table td').eq(1).text(device.name);
+	$('#device table td').eq(1).text(navigator.device.name);
     $('#device table td').eq(3).text(device.phonegap);
 	$('#device table td').eq(5).text(device.plataform);
-	$('#device table td').eq(7).text('device.version');
+	$('#device table td').eq(7).text(device.version);
 	$('#device table td').eq(7).text(device.uuid);
 }
 
