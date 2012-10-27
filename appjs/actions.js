67224cb73b36604c67a63d5d1479a45b610f31c7
Eon Matrix <matrix_eon@yahoo.com>
2012-10-27 11:49:37 -0500

    N7

--- appjs/actions.js
+++ appjs/actions.js
@@ -1,4 +1,5 @@
 // JavaScript Document
+var jQT = $.jQTouch()
 function get_device_info(){
 	$('#device table td').eq(1).text(navigator.device.name);
     $('#device table td').eq(3).text(device.phonegap);
